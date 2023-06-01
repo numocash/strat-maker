2bc280b01e32ff75a632899af587e8cbd43e69eb
Robert Leifke <r.leifke@gmail.com>
2023-06-01 14:01:35 +0200

    Added to position.sol

--- src/core/Position.sol
+++ src/core/Position.sol
@@ -4,5 +4,17 @@ pragma solidity ^0.8.19;
 library Position {
     struct Info {
         uint256 liquidity;
+        uint256 tickInside0Last;
+        uint256 tickInside1Last;
+        uint256 limitOrderTyper;
+        uint256 settlementSnapshotID;
+    }
+
+    function get(mapping(bytes32 => Position) storage positions,
+                    uint8 tierId,
+                    int24 tickLower,
+                    int24 tickUpper
+                    ) internal view returns (Position storage position) {
+        position = positions[keccak256(abi.encodePacked(tierId, tickLower, tickUpper))];
     }
 }
