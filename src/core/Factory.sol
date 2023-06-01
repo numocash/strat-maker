0dc1d8214487b59f62c7c6f5271603403de020a5
Robert Leifke <r.leifke@gmail.com>
2023-06-01 13:44:33 +0200

    Added sorting

--- src/core/Factory.sol
+++ src/core/Factory.sol
@@ -1,31 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.19;
 
-import { Pair } from "./Pair.sol";
+import {Pair} from "./Pair.sol";
 
 contract Factory {
+    /*//////////////////////////////////////////////////////////////
+                        EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    event PairCreated(address indexed tokenA, address indexed tokenB, int24 indexed tick, address pair);
 
     /*//////////////////////////////////////////////////////////////
                         ERRORS
     //////////////////////////////////////////////////////////////*/
 
-  error SameTokenError();
-
-  error ZeroAddressError();
-
-  error TickZeroError();
-
-  error DeployedError();
-
-  error ScaleError();
+    error SameTokenError();
+    error ZeroAddressError();
+    error DeployedError();
 
     /*//////////////////////////////////////////////////////////////
-                        STOARGE
+                        STORAGE
     //////////////////////////////////////////////////////////////*/
 
     // Three-level mapping
     mapping(address => mapping(address => mapping(int24 => address))) public getPair;
-    
     mapping(uint8 => int24) public tierAmountTick;
 
     /*//////////////////////////////////////////////////////////////
@@ -36,30 +34,25 @@ contract Factory {
         address token0;
         address token1;
         int24 tick;
-        uint8 tier;
     }
 
     Parameters public parameters;
 
-    function createPair(
-        address token0,
-        address token1,
-        uint8 tier
-    ) external returns (address pair) {
-        if (token0 == token1) revert SameTokenError();
-        if (token0 == address(0) || token1 == address(0)) revert ZeroAddressError();
-        if (tierAmountTick[tier] != 0) revert TickZeroError();
-        if (getPair[token0][token1][tier] != address(0)) revert DeployedError();
+    function createPair(address tokenA, address tokenB) external returns (address pair) {
+        if (tokenA == tokenB) revert SameTokenError();
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        if (tokenA == address(0) || tokenB == address(0)) revert ZeroAddressError();
+        if (getPair[token0][token1][0] != address(0)) revert DeployedError();
 
-        parameters = Parameters({ token0: token0, token1: token1, tick: tick, tier: tier });
+        parameters = Parameters(tokenA, tokenB, 0);
 
-        pair = address(new Pair{ salt: keccak256(abi.encode(token0, token1, tier)) }());
-
+        //Error 
+        pair = address(new Pair{salt: keccak256(abi.encode(token0, token1, 0)) }(token0, token1, 0));
         delete parameters;
 
-        getPair[token0][token1][tier] = pair;
+        getPair[token0][token1][0] = pair;
+        getPair[token1][token0][0] = pair;
 
-        // emit PairCreated(token0, token1, tick, tier, pair);
-  }
+        emit PairCreated(tokenA, tokenB, 0, pair);
+    }
 }
-
