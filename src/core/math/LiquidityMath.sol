0bf7103511ae026f3d59298d47231743761a5fbe
Kyle Scott <kyscott@umich.edu>
2023-06-15 09:27:55 -0400

    round up math

--- src/core/math/LiquidityMath.sol
+++ src/core/math/LiquidityMath.sol
@@ -1,21 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.19;
 
-import {mulDiv} from "./FullMath.sol";
+import {mulDiv, mulDivRoundingUp} from "./FullMath.sol";
 import {getRatioAtStrike, Q128} from "./StrikeMath.sol";
 
 /// @notice Calculate amount0 delta when moving completely through the liquidity at the strike.
 /// @dev Assumes inputs are valid
 /// i.e. x = L/Pi
 /// @custom:team Rounding needs to be checked
-function getAmount0Delta(uint256 liquidity, int24 strike) pure returns (uint256 amount0) {
-    return mulDiv(liquidity, Q128, getRatioAtStrike(strike));
+function getAmount0Delta(uint256 liquidity, int24 strike, bool roundUp) pure returns (uint256 amount0) {
+    return roundUp
+        ? mulDivRoundingUp(liquidity, Q128, getRatioAtStrike(strike))
+        : mulDiv(liquidity, Q128, getRatioAtStrike(strike));
 }
 
 /// @notice Calculate amount1 delta when moving completely through the liquidity at the strike.
 /// @dev Assumes inputs are valid
 /// i.e. y = L
-/// @custom:team Rounding needs to be checked
 function getAmount1Delta(uint256 liquidity) pure returns (uint256 amount1) {
     return liquidity;
 }
@@ -25,17 +26,32 @@ function getAmount1Delta(uint256 liquidity) pure returns (uint256 amount1) {
 function getAmount0FromComposition(
     uint128 composition,
     uint256 liquidity,
-    uint256 ratioX128
+    uint256 ratioX128,
+    bool roundUp
 )
     pure
     returns (uint256 amount0)
 {
-    return mulDiv(liquidity, (type(uint128).max - composition), ratioX128);
+    uint128 token0Composition;
+    unchecked {
+        token0Composition = type(uint128).max - composition;
+    }
+
+    return roundUp
+        ? mulDivRoundingUp(liquidity, token0Composition, ratioX128)
+        : mulDiv(liquidity, token0Composition, ratioX128);
 }
 
 /// @notice Calculate amount0 in a strike for a given composition and liquidity
-function getAmount1FromComposition(uint128 composition, uint256 liquidity) pure returns (uint256 amount1) {
-    return mulDiv(liquidity, composition, Q128);
+function getAmount1FromComposition(
+    uint128 composition,
+    uint256 liquidity,
+    bool roundUp
+)
+    pure
+    returns (uint256 amount1)
+{
+    return roundUp ? mulDivRoundingUp(liquidity, composition, Q128) : mulDiv(liquidity, composition, Q128);
 }
 
 /// @notice Calculate amount{0,1} needed for the given liquidity change
@@ -45,19 +61,20 @@ function calcAmountsForLiquidity(
     int24 strikeCurrent,
     uint128 composition,
     int24 strike,
-    uint256 liquidity
+    uint256 liquidity,
+    bool roundUp
 )
     pure
     returns (uint256 amount0, uint256 amount1)
 {
     if (strike > strikeCurrent) {
-        return (getAmount0Delta(liquidity, strike), 0);
+        return (getAmount0Delta(liquidity, strike, roundUp), 0);
     } else if (strike < strikeCurrent) {
         return (0, getAmount1Delta(liquidity));
     } else {
         return (
-            getAmount0FromComposition(composition, liquidity, getRatioAtStrike(strike)),
-            getAmount1FromComposition(composition, liquidity)
+            getAmount0FromComposition(composition, liquidity, getRatioAtStrike(strike), roundUp),
+            getAmount1FromComposition(composition, liquidity, roundUp)
         );
     }
 }
@@ -66,10 +83,10 @@ function calcAmountsForLiquidity(
 function calcLiquidityForAmounts(
     int24 strikeCurrent,
     uint128 composition,
-    int24 strikeLower,
-    int24 strikeUpper,
+    int24 strike,
     uint256 amount0,
-    uint256 amount1
+    uint256 amount1,
+    bool roundUp
 )
     pure
     returns (uint256 liquidity)
