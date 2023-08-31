d1ba3a816a2de7aa1ff26f9cedaa45c44ef3d858
Kyle Scott <kyscott@umich.edu>
2023-08-31 10:13:15 -0400

    accrue tests

--- evm/test/unit/concrete/Engine/accrue.t.sol
+++ evm/test/unit/concrete/Engine/accrue.t.sol
@@ -2,51 +2,51 @@
 pragma solidity ^0.8.0;
 
 import {Test} from "forge-std/Test.sol";
-import {accrueCommand, createCommand, createCommandInput, pushCommandInputs} from "../../../utils/Commands.sol";
 
 import {Engine} from "src/core/Engine.sol";
-import {Accounts} from "src/core/Accounts.sol";
 import {Pairs} from "src/core/Pairs.sol";
 
-contract AccrueTest is Test {
-    event Accrue(bytes32 indexed pairID, int24 indexed strike, uint136 liquidityAccrued);
+contract AccrueTest is Test, Engine {
+    using Pairs for Pairs.Pair;
+    using Pairs for mapping(bytes32 => Pairs.Pair);
 
-    Engine private engine;
-
-    function setUp() external {
-        engine = new Engine();
-    }
-
-    function test_Accrue_Zero() external {
+    function test_Accure_Zero() external {
         vm.pauseGasMetering();
 
-        Engine.CommandInput[] memory commandInputs = createCommandInput();
-        commandInputs = pushCommandInputs(commandInputs, createCommand(address(1), address(2), 0, 0));
-
-        engine.execute(address(0), commandInputs, 0, 0, bytes(""));
-
-        delete commandInputs;
-
-        commandInputs = pushCommandInputs(commandInputs, accrueCommand(address(1), address(2), 0, 0));
-
-        vm.expectEmit(true, true, false, true);
-        emit Accrue(Pairs.getPairID(address(1), address(2), 0), 0, 0);
+        (bytes32 pairID, Pairs.Pair storage pair) = pairs.getPairAndID(address(1), address(2), 0);
+        pair.initialize(0);
+        pair.addSwapLiquidity(0, 1, 1e18);
 
         vm.resumeGasMetering();
 
-        Accounts.Account memory account = engine.execute(address(0), commandInputs, 0, 0, bytes(""));
+        _accrue(Engine.AccrueParams(pairID, 0));
 
         vm.pauseGasMetering();
 
-        assertEq(account.erc20Data.length, 0);
-        assertEq(account.lpData.length, 0);
-
-        Pairs.Strike memory strike = engine.getStrike(address(1), address(2), 0, 0);
-
-        assertEq(strike.blockLast, 1);
+        assertEq(pair.strikes[0].blockLast, 1);
+        assertEq(pair.strikes[0].liquidity[0].swap, 1e18);
 
         vm.resumeGasMetering();
     }
 
-    function test_Accrue_NonZero() external {}
+    function test_Accure_NonZero() external {
+        vm.pauseGasMetering();
+
+        (bytes32 pairID, Pairs.Pair storage pair) = pairs.getPairAndID(address(1), address(2), 0);
+        pair.initialize(0);
+        pair.addSwapLiquidity(0, 1, 1e18);
+        pair.addBorrowedLiquidity(0, 0.5e18);
+
+        vm.resumeGasMetering();
+
+        _accrue(Engine.AccrueParams(pairID, 0));
+
+        vm.pauseGasMetering();
+
+        assertEq(pair.strikes[0].blockLast, 1);
+        assertEq(pair.strikes[0].liquidity[0].swap, 0.5e18 + 0.5e18 / 10_000);
+        assertEq(pair.strikes[0].liquidity[0].borrowed, 0.5e18 - 0.5e18 / 10_000);
+
+        vm.resumeGasMetering();
+    }
 }
