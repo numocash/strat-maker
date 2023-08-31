a9bbf288ce622e1fecf5a67c52156e6eb4b78aba
Kyle Scott <kyscott@umich.edu>
2023-08-31 09:54:53 -0400

    remove liquidity test

--- evm/test/unit/concrete/Engine/removeLiquidity.t.sol
+++ evm/test/unit/concrete/Engine/removeLiquidity.t.sol
@@ -6,7 +6,7 @@ import {Test} from "forge-std/Test.sol";
 import {Accounts} from "src/core/Accounts.sol";
 import {Engine} from "src/core/Engine.sol";
 import {Pairs} from "src/core/Pairs.sol";
-import {Positions, biDirectionalID} from "src/core/Positions.sol";
+import {biDirectionalID} from "src/core/Positions.sol";
 
 import {getAmounts} from "src/core/math/LiquidityMath.sol";
 import {Q128} from "src/core/math/StrikeMath.sol";
@@ -191,11 +191,35 @@ contract RemoveLiquidityTest is Test, Engine {
 
         vm.pauseGasMetering();
 
-        assertEq(pair.strikes[2].liquidity[0].swap, 0);
-        assertEq(pair.strikes[2].liquidity[0].borrowed, 0);
+        (uint256 amount0,) = getAmounts(pair, 1e18, 2, 1, false);
+
+        assertEq(account.erc20Data[0].token, address(1));
+        assertEq(account.erc20Data[0].balanceDelta, -int256(amount0));
 
         vm.resumeGasMetering();
     }
 
-    function test_RemoveLiquidity_AmountsScale() external {}
+    function test_RemoveLiquidity_AmountsScale() external {
+        vm.pauseGasMetering();
+
+        (, Pairs.Pair storage pair) = pairs.getPairAndID(address(1), address(2), 8);
+        Accounts.Account memory account = Accounts.newAccount(1, 1);
+        pair.initialize(0);
+
+        pair.addSwapLiquidity(2, 1, 1e18);
+        _mintBiDirectional(address(this), address(1), address(2), 8, 2, 1, 1e18);
+
+        vm.resumeGasMetering();
+
+        _removeLiquidity(Engine.RemoveLiquidityParams(address(1), address(2), 8, 2, 1, 1e18), account);
+
+        vm.pauseGasMetering();
+
+        (uint256 amount0,) = getAmounts(pair, 1e18 << 8, 2, 1, false);
+
+        assertEq(account.erc20Data[0].token, address(1));
+        assertEq(account.erc20Data[0].balanceDelta, -int256(amount0));
+
+        vm.resumeGasMetering();
+    }
 }
