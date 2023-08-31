54e5f2a0ac3c5575cf85813907e0c7ace612ed59
Kyle Scott <kyscott@umich.edu>
2023-08-31 09:45:23 -0400

    router impl

--- evm/src/periphery/Router.sol
+++ evm/src/periphery/Router.sol
@@ -1,78 +1,80 @@
-// // SPDX-License-Identifier: GPL-3.0-only
-// pragma solidity ^0.8.19;
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.19;
 
-// import {Engine} from "src/core/Engine.sol";
-// import {Positions} from "src/core/Positions.sol";
-// import {IExecuteCallback} from "src/core/interfaces/IExecuteCallback.sol";
-// import {ILRTA} from "ilrta/ILRTA.sol";
-// import {Permit3} from "ilrta/Permit3.sol";
+import {Accounts} from "src/core/Accounts.sol";
+import {Engine} from "src/core/Engine.sol";
+import {IExecuteCallback} from "src/core/interfaces/IExecuteCallback.sol";
+import {ILRTA} from "ilrta/ILRTA.sol";
+import {Permit3} from "ilrta/Permit3.sol";
 
-// /// @author Robert Leifke and Kyle Scott
-// /// @custom:team route by signature
-// contract Router is IExecuteCallback {
-//     Engine private immutable engine;
-//     Permit3 private immutable permit3;
+/// @author Robert Leifke and Kyle Scott
+contract Router is IExecuteCallback {
+    Engine private immutable engine;
+    Permit3 private immutable permit3;
 
-//     error InvalidCaller(address caller);
+    error InvalidCaller(address caller);
 
-//     struct CallbackData {
-//         Permit3.TransferDetails[] permitTransfers;
-//         address payer;
-//         bytes signature;
-//     }
+    struct CallbackData {
+        address payer;
+        Permit3.SignatureTransferBatch signatureTransfer;
+        bytes signature;
+    }
 
-//     constructor(address _engine, address _permit3) {
-//         engine = Engine(_engine);
-//         permit3 = Permit3(_permit3);
-//     }
+    constructor(address _engine, address _permit3) {
+        engine = Engine(_engine);
+        permit3 = Permit3(_permit3);
+    }
 
-//     struct RouteParams {
-//         address to;
-//         Engine.Commands[] commands;
-//         bytes[] inputs;
-//         uint256 numTokens;
-//         uint256 numLPs;
-//         Permit3.TransferDetails[] permitTransfers;
-//         bytes signature;
-//     }
+    struct RouteParams {
+        address to;
+        Engine.CommandInput[] commandInputs;
+        uint256 numTokens;
+        uint256 numLPs;
+        Permit3.SignatureTransferBatch signatureTransfer;
+        bytes signature;
+    }
 
-//     function route(RouteParams calldata params) external {
-//         CallbackData memory callbackData = CallbackData(params.permitTransfers, msg.sender, params.signature);
+    function route(RouteParams calldata params) external {
+        CallbackData memory callbackData = CallbackData(msg.sender, params.signatureTransfer, params.signature);
 
-//         return engine.execute(
-//             params.to, params.commands, params.inputs, params.numTokens, params.numLPs, abi.encode(callbackData)
-//         );
-//     }
+        engine.execute(params.to, params.commandInputs, params.numTokens, params.numLPs, abi.encode(callbackData));
+    }
 
-//     function executeCallback(IExecuteCallback.CallbackParams calldata params) external {
-//         if (msg.sender != address(engine)) revert InvalidCaller(msg.sender);
-//         CallbackData memory callbackData = abi.decode(params.data, (CallbackData));
+    function executeCallback(Accounts.Account calldata account, bytes calldata data) external {
+        unchecked {
+            if (msg.sender != address(engine)) revert InvalidCaller(msg.sender);
+            CallbackData memory callbackData = abi.decode(data, (CallbackData));
 
-//         // build array of transfer requests, then send as a batch
-//         Permit3.RequestedTransferDetails[] memory requestedTransfer =
-//             new Permit3.RequestedTransferDetails[](callbackData.permitTransfers.length);
+            // build array of transfer requests, then send as a batch
+            Permit3.RequestedTransferDetails[] memory requestedTransfer =
+                new Permit3.RequestedTransferDetails[](callbackData.signatureTransfer.transferDetails.length);
 
-//         uint256 j = 0;
-//         for (uint256 i = 0; i < params.tokensDelta.length;) {
-//             int256 delta = params.tokensDelta[i];
+            uint256 j = 0;
 
-//             if (delta > 0 && params.tokens[i] != address(0)) {
-//                 requestedTransfer[j] = Permit3.RequestedTransferDetails(msg.sender, uint256(delta));
+            // Format ERC20 data
+            for (uint256 i = 0; i < account.erc20Data.length; i++) {
+                int256 delta = account.erc20Data[i].balanceDelta;
 
-//                 unchecked {
-//                     j++;
-//                 }
-//             }
+                // NOTE: Second expression might be unecessary
+                if (delta > 0 && account.erc20Data[i].token != address(0)) {
+                    // NOTE: How to make sure this is indexing into the right spot
+                    requestedTransfer[j] = Permit3.RequestedTransferDetails(msg.sender, abi.encode(uint256(delta)));
 
-//             unchecked {
-//                 i++;
-//             }
-//         }
+                    j++;
+                }
+            }
 
-//         if (callbackData.permitTransfers.length > 0) {
-//             permit3.transferBySignature(
-//                 callbackData.payer, callbackData.permitTransfers, requestedTransfer, callbackData.signature
-//             );
-//         }
-//     }
-// }
+            // Format position data
+            for (uint256 i = 0; i < account.lpData.length; i++) {
+                requestedTransfer[j] = Permit3.RequestedTransferDetails(msg.sender, abi.encode(account.lpData[i]));
+                j++;
+            }
+
+            if (callbackData.signatureTransfer.transferDetails.length > 0) {
+                permit3.transferBySignature(
+                    callbackData.payer, callbackData.signatureTransfer, requestedTransfer, callbackData.signature
+                );
+            }
+        }
+    }
+}
